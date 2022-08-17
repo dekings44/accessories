8b16eb1e0bb991f21a4550fda35492e48d5ada00
Kingsley Ihemere <dekings44@gmail.com>
2022-08-17 21:51:26 +0100

    first accessories commit

--- test.py
+++ test.py
@@ -16,44 +16,60 @@ def get_url(search_term):
 
 url = get_url('smart tv')
 
-print(url)
 
 header = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 req = requests.get(url, headers = header).text
 
-soup = BeautifulSoup(req, 'html5lib')
-# print(soup)
-# print(type(soup))
+def getNextPage(soup):
+    page = soup.find('ul', {'class': 'a-pagination'})
+    if not page.find('li', {'class': 'a-disable a-last'}):
+        url = 'https://www.amazon.co.uk' + str(page.find('li', {'class': 'a-last'}).find('a')['href'])
+        return url
+    else:
+        return
 
-# Gettig The names of the accessories
-card = soup.find_all('span', {'class' : 'a-size-medium a-color-base a-text-normal'})
+while True:
+    soup = BeautifulSoup(req, 'html5lib')
+    page = getNextPage(soup)
 
-name = [comp.text for comp in card]
-print(name)
+    # print(soup)
+    # print(type(soup))
 
-#Getting the ratings of each accessory
-rating = soup.find_all('span', {'class' : 'a-icon-alt'})
-rating = [comp.text for comp in rating]
-print(rating)
+    # Gettig The names of the accessories
+    card = soup.find_all('span', {'class' : 'a-size-medium a-color-base a-text-normal'})
 
-#Getting the total rating of each accessory
-total_rating = soup.find_all('span', {'class' : 'a-size-base s-underline-text'})
-total_rating = [comp.text for comp in total_rating]
-print(total_rating)
+    name = [comp.text for comp in card]
+    print(name)
+
+    #Getting the ratings of each accessory
+    rating = soup.find_all('span', {'class' : 'a-icon-alt'})
+    rating = [comp.text for comp in rating]
+    print(rating)
+
+    #Getting the total rating of each accessory
+    total_rating = soup.find_all('span', {'class' : 'a-size-base s-underline-text'})
+    total_rating = [comp.text for comp in total_rating]
+    print(total_rating)
 
 
-#Getting the pounds side of the price
-price_whole = soup.find_all('span', {'class' : 'a-price-whole'})
-price_whole = [comp.text for comp in price_whole]
-print(price_whole)
+    #Getting the pounds side of the price
+    price_whole = soup.find_all('span', {'class' : 'a-price-whole'})
+    price_whole = [comp.text for comp in price_whole]
+    print(price_whole)
 
-#Getting the pence side of the price
-price_dec = soup.find_all('span', {'class' : 'a-price-fraction'})
-price_dec = [comp.text for comp in price_dec]
-print(price_dec)
+    #Getting the pence side of the price
+    price_dec = soup.find_all('span', {'class' : 'a-price-fraction'})
+    price_dec = [comp.text for comp in price_dec]
+    print(price_dec)
+
+    if not page:
+        break
+    print(page)
 
 
 #Getting the image of the accessory
 # images = soup.find_all('img', {'class' : 's-image'})
 # images = [comp.src for comp in images]
 # print(images)
+
+
