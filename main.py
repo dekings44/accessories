7b0b6d22a2dc35829196b6d1ee379b0f0d5908cb
Kingsley Ihemere <dekings44@gmail.com>
2022-08-20 14:45:20 +0100

    first accessories commit

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
-from requests_html import HTMLSession
+#importing the libraries needed 
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+from random import randint
 
-def getPrices(url):
-    s = HTMLSession()
-    r = s.get(url)
-    r.html.render(sleep=1)
+#Declaring the headers 
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 
-    product = {
-        'title': r.html.xpath('//*[@id="productTitle"]', first=True).text,
-        'price': r.html.xpath('//*[@id="priceblock_ourprice"]', first=True).text
-    }
+#declaring the list of empty variables, So that we can append the data overall
 
-    print(product)
-    return product
+acc_name = []
+rating = []
+total_rating=[]
+price_whole=[]
+price_dec =[]
 
 
-getPrices('https://www.amazon.co.uk/iPhone-Charger-Cable-Lightning-Compatible/dp/B08F2NDB39?ref_=Oct_d_oup_d_560800&pd_rd_w=TtYu9&content-id=amzn1.sym.cf168abf-8d77-4933-98b1-3151f5974581&pf_rd_p=cf168abf-8d77-4933-98b1-3151f5974581&pf_rd_r=29D7F0H9VJZ4XKR3P81Q&pd_rd_wg=AsMvu&pd_rd_r=dc5ed78e-9d5d-434b-955b-92b1aeaaa3b5&pd_rd_i=B08F2NDB39')
+#creating an array of values and passing it in the url for dynamic webpages
+pages = np.arange(1,20,1)
+
+#the whole core of the script
+for page in pages:
+    page = requests.get("https://www.amazon.co.uk/s?k=computer+accessory&page="+str(page)+"&crid=35AY920YDA85G&qid=1660999890&sprefix=%2Caps%2C71&ref=sr_pg_2")
+    soup = BeautifulSoup(page.text, 'html.parser')
+    acc_data = soup.findAll('div', attrs = {'data-component-type': 's-search-result'})
+    sleep(randint(2,8))
+    for store in acc_data:
+        name = store.find('span', {'class' : 'a-size-medium a-color-base a-text-normal'}).text
+        acc_name.append(name)
+        
+        acc_rating = store.find('span', {'class' : 'a-icon-alt'}).text
+        rating.append(acc_rating)
+        
+        acc_total_rating = store.find('span', {'class' : 'a-size-base s-underline-text'}).text
+        total_rating.append(acc_total_rating)
+        
+        acc_price_whole = store.find('span', {'class' : 'a-price-whole'}).text
+        price_whole.append(acc_price_whole)
+        
+        acc_price_dec = store.find('span', {'class' : 'a-price-fraction'}).text
+        price_dec.append(acc_price_dec)
+        
+        
+#creating a dataframe 
+acc_list = pd.DataFrame({ "Name": acc_name, "Rating" : rating, "Total Rating": total_rating, "Price Pounds": price_whole, "Price Pence" : price_dec})
+
+print(acc_list.head())
